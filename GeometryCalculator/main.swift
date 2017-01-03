7491aff3c092df68a3ee991f23701fb350cbadba
ranokim <daesub0515@gmail.com>
2017-01-04 03:00:50 +0900

    first commit

---
+++ GeometryCalculator/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  GeometryCalculator
+//
+//  Created by Park on 9/18/16.
+//  Copyright © 2016 Park. All rights reserved.
+//
+
+import Foundation
+
+// main
+let calc = GeometryCalculator()
+calc.printAllGeometrySurfaceAreaVolume()
+calc.calculateUserInputGeometrySurfaceAreaVolume()
+
+
+
